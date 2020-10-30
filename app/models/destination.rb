de1b8f5733bcbd0358dc1d4a99d5ee63b1a0dfa8
AlecGrey <alecgrey.coding@gmail.com>
2020-10-30 14:16:41 -0700

    validators complete

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -3,4 +3,7 @@ class Destination < ApplicationRecord
     has_many :posts
     has_many :bloggers, through: :posts
 
+    validates :name, presence: true, uniqueness: true
+    validates :country, presence: true, uniqueness: true
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,11 @@
 class Post < ApplicationRecord
 
     belongs_to :blogger
-    belongs_to :destination   
+    belongs_to :destination
+    
+    validates :title, presence: true
+    validates :content, length: {minimum: 100}
+    validates :blogger_id, presence: true
+    validates :destination_id, presence: true
     
 end
